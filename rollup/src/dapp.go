a54d64dc467c441597fb2a0f22bf81651827990e
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-06-08 14:39:27 -0300

    created rollup

---
+++ rollup/src/dapp.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+  "dapp/rollups"
+	"github.com/rollmelette/rollmelette"
+)
+
+type ColdWheels struct {}
+
+func NewColdWheels() *ColdWheels {
+	return &ColdWheels{}
+}
+
+
+
+
+func (d *ColdWheels) Advance(
+	env rollmelette.Env,
+	metadata rollmelette.Metadata,
+	deposit rollmelette.Deposit,
+	payload []byte,
+) error {
+  fmt.Printf("Raw payload: %s\n", string(payload))
+	// fmt.Printf("Raw payload: %s\n", string(payload))
+	var input *rollups.AdvaceInputDTO
+	err := json.Unmarshal(payload, &input)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal input payload: %w", err)
+	}
+  // log.Printf("Input: %v\n", input)
+  return nil
+}
+
+func (d *ColdWheels) Inspect(env rollmelette.EnvInspector, payload []byte) error {
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	opts := rollmelette.NewRunOpts()
+	app := NewColdWheels()
+	err := rollmelette.Run(ctx, opts, app)
+	if err != nil {
+		slog.Error("application error", "error", err)
+	}
+}
---
+++ rollup/src/go.mod
@@ -0,0 +1,16 @@
+module dapp
+
+go 1.22.4
+
+require github.com/rollmelette/rollmelette v0.1.1
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.13.8 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/lmittmann/tint v1.0.3 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
