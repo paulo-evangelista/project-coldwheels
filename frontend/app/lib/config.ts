13d2cf26f811a015da766a63a2d36b99ff48d5b9
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-06-09 04:17:10 -0300

    feat(frontend): :sparkles: adding cartesi interactions

--- frontend/app/lib/config.ts
+++ frontend/app/lib/config.ts
@@ -6,7 +6,18 @@ import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
 
 const projectId = 'b934d56b44c8b458bc65034dea081498';
 
-const supportedChains: Chain[] = [sepolia, bscTestnet];
+const coldWheels = {
+   id: 31337,
+   name: 'ColdWheels',
+   iconUrl: 'https://logos-world.net/wp-content/uploads/2021/08/Hot-Wheels-Logo.png',
+   iconBackground: '#fff',
+   nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 },
+   rpcUrls: {
+     default: { http: ['http://127.0.0.1:8545'] },
+   },
+ } as const satisfies Chain;
+
+const supportedChains: Chain[] = [sepolia, bscTestnet, coldWheels];
 
 export const config = getDefaultConfig({
    appName: 'WalletConnection',
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,13 +1,11 @@
 'use client'
-import React, { useState } from "react";
+import React from "react";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
-import { ethers } from 'ethers';
-import { useWallets } from "@web3-onboard/react";
-import { advanceDAppRelay, advanceERC20Deposit, advanceERC721Deposit, advanceEtherDeposit, advanceInput } from 'cartesi-client';
 import { init } from "@web3-onboard/react";
 import configFile from "./config.json";
 import injectedModule from "@web3-onboard/injected-wallets";
+import { Network } from "./network";
 
 const injected = injectedModule();
 init({
@@ -24,6 +22,7 @@ init({
     description: "Decentralized Marketplace for Adspaces",
     recommendedInjectedWallets: [
       { name: "MetaMask", url: "https://metamask.io" },
+      { name: "MetaMask", url: "https://metamask.io" },
     ],
   },
 });
@@ -33,55 +32,12 @@ interface IInputPropos {
 }
 
 const Login: NextPage<IInputPropos> = (props) => {
-  const [connectedWallet] = useWallets();
-  const [input, setInput] = useState<string>("");
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const [hexInput, setHexInput] = useState<boolean>(false);
-
-  const dappAddress = props.dappAddress;
-  console.log("dapp address is:", dappAddress);
-  const addInput = async () => {
-      console.log("adding input", input);
-      const signer = await provider.getSigner();
-      console.log("signer and input is ", signer, input);
-      advanceInput(signer, dappAddress, input);
-  };
-
-  const sendAddress = async () => {
-      console.log("sending dapp address");
-      const signer = await provider.getSigner();
-      advanceDAppRelay(signer, dappAddress);
-  }
-
   return (
     <div>
       <main>
         <ConnectButton />
           <div>
-            Send Address (send relay dapp address) <br />
-            <button onClick={() => sendAddress()} >
-                Send
-            </button>
-            <br />
-            <br />
-          </div>
-          <div>
-            Send Input <br />
-            Input:{" "}
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <input
-                type="checkbox"
-                checked={hexInput}
-                onChange={(e) => setHexInput(!hexInput)}
-            />
-            <span>Raw Hex </span>
-            <button onClick={() => addInput()}>Send</button>
-            <br />
-            <br />
+            <Network></Network>
           </div>
       </main>
     </div>
