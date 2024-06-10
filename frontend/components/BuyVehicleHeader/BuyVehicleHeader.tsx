c7affa3d4966b5c037ee90bbcc3c2d25fc69fd4a
Gabriel Farias <gabrielfalves2003@gmail.com>
2024-06-09 22:27:21 -0300

    fix: header sizing

--- frontend/components/BuyVehicleHeader/BuyVehicleHeader.tsx
+++ frontend/components/BuyVehicleHeader/BuyVehicleHeader.tsx
@@ -14,12 +14,12 @@ export default function BuyVehicleHeader() {
                     <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
                     <Input
                         placeholder="Digite a placa do veículo"
-                        className="w-full h-16 pl-10 rounded-md bg-[#F3F3F3] border-none"
+                        className="w-full h-16 pl-10 rounded-lg bg-[#F3F3F3] border-none"
                     />
                 </div>
             </div>
 
-            <div className="w-2/12 h-16 flex items-center justify-center">
+            <div className="w-2/12 h-16 flex items-center justify-end">
                 <Button className="w-8/12 h-16">Pesquisar</Button>
             </div>
         </div>
