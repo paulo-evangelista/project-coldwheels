1f9539be88446f932a1679f7787468f66b86d6e4
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-06-13 00:55:49 -0300

    feat(component): :sparkles: create upload image to ipfs component

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -10,48 +10,8 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 export default function BuyVehiclePage({ }) {
-    const [isChecking, setIsChecking] = useState<boolean>(true);
-    const { address: wallet, isConnected } = useAccount();
-    // const router = useRouter();
-
-    // const getWallet = async () => {
-    //     var accountCreatedStatus = false
-    //     const response = await axios.post('http://localhost:8080/inspect', { "kind": "company", "payload": { wallet } }).then((response) => {
-    //         return response;
-    //     }).catch((error) => {
-    //         console.error("error is ", error);
-    //         return error;
-    //     });
-
-    //     if (response.data.status === "Accepted") {
-    //         accountCreatedStatus = true;
-    //     } else {
-    //         accountCreatedStatus = false;
-    //     }
-
-    //     return accountCreatedStatus;
-    // }
-
-    // useEffect(() => {
-    //     const checkAccountStatus = async () => {
-    //         if (!isConnected) {
-    //             const accountExists = await getWallet();
-    //             setIsChecking(accountExists);
-    //             if (accountExists) {
-    //                 toast.info("Redirecting to dashboard...");
-    //                 // setTimeout(() => {
-    //                 //     router.push('/home');
-    //                 // }, 1500);
-    //             }
-    //         }
-    //     };
-    //     checkAccountStatus();
-    // }, [isConnected]);
-
     return (
         <div className="h-screen px-14 flex flex-col">
-            {/* <div className="flex h-screen bg-white"> */}
-            {/* <Sidebar /> */}
             <div className="flex-shrink-0">
                 <Header />
             </div>
