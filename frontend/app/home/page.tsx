07c61ebbafa8794d60eb021b70a4ed1bafb1db66
Gabriel Farias <gabrielfalves2003@gmail.com>
2024-06-15 15:09:24 -0300

    pagina sem carro

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -8,16 +8,19 @@ import { useRouter } from "next/router";
 import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
 
-export default function BuyVehiclePage({ }) {
+export default function BuyVehiclePage({}) {
     return (
-        <div className="h-screen px-14 flex flex-col">
+        <div className="h-screen px-14 flex flex-col bg-[#E1E8F0]">
             <div className="flex-shrink-0">
                 <Header />
             </div>
             <div className="flex justify-between flex-grow overflow-hidden">
                 <Sidebar />
-                <Details />
-                <VehicleEvents />
+                <div className="flex flex-grow items-center justify-center text-2xl text-black">
+                    <div className=" bg-white p-4 rounded-xl shadow-lg">
+                        Search for a vehicle
+                    </div>
+                </div>
             </div>
         </div>
     );
